cf09699c64ef7599e2c7210ef0742b8e8134c9a1
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 00:28:01 +0500

    Swap Values

---
+++ SwapValues.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapValues
+{
+	internal class SwapValues
+	{
+		static void Main(string[] args)
+		{
+			string secondName = "Петя";
+			string firstName = "Иванов";
+			string temp;
+
+			Console.WriteLine($"До перестановки: фамилия - {secondName}, имя - {firstName}");
+
+			temp = secondName;
+			secondName = firstName;
+			firstName = temp;
+			Console.WriteLine($"После перестановки: фамилия - {secondName}, имя - {firstName}");
+		}
+	}
+}
